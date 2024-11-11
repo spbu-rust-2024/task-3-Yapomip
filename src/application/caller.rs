17948098d0e31998e7dcd307ed36146421f1f738
Yapomip <zenovaromir@gmail.com>
2024-11-11 23:16:34 +0300

    Task 3 resomething sample 2

--- src/application/caller.rs
+++ src/application/caller.rs
@@ -14,20 +14,50 @@ macro_rules! function_string_name {
     };
 }
 
+macro_rules! if_some_macro {
+    ( $data:ident, $parametrs:ident, $param_string:ident : $if_some:expr => $else_some:expr ) => {
+        if $data.len() > 0 {
+            for param_option in $parametrs {
+                if let Some($param_string) = param_option {
+                    $if_some;
+                } else {
+                    $else_some;
+                }
+            }
+        }
+    };
+    ( $data:ident, $parametrs:ident : $if_some:expr , $else_some:expr ) => {
+        if $data.len() > 0 {
+            for param_option in $parametrs {
+                if let Some(param_string) = param_option {
+                    $if_some;
+                } else {
+                    $else_some;
+                }
+            }
+        }
+    };
+}
+
 macro_rules! create_call_function {
     ( $function_name:ident, $func_string_name:expr, $func:expr ) => {
         fn $function_name(data: &[i64], parametrs: &Vec<Option<&str>>) -> String {
             let mut answer = String::new();
             
-            if data.len() > 0 {
-                for param_option in parametrs {
-                    if let Some(param_string) = param_option {
-                        answer += &error(format!("'{}' : have no parametr '{}'!\n", $func_string_name, param_string));
-                    } else {
-                        answer += &format!("{} : {}\n", $func_string_name, $func(data));
-                    }
-                }
-            }
+            if_some_macro!(data, parametrs, param_string :
+                answer += &error(format!("'{}' : have no parametr '{}'!\n", $func_string_name, param_string))
+                =>
+                answer += &format!("{} : {}\n", $func_string_name, $func(data))
+            );
+            // if data.len() > 0 {
+            //     for param_option in parametrs {
+            //         if let Some(param_string) = param_option {
+            //             answer += &error(format!("'{}' : have no parametr '{}'!\n", $func_string_name, param_string));
+            //         } else {
+            //             answer += &format!("{} : {}\n", $func_string_name, $func(data));
+            //         }
+            //     }
+            // }
             
             answer
         }
@@ -66,48 +96,36 @@ fn call_generalized_mean(data: &[i64], parametrs: &Vec<Option<&str>>) -> String 
     let mut answer = String::new();
     let function_name: String = function_string_name!(generalized_mean);
     
-    if data.len() > 0 {
-        for param_option in parametrs {
-            if let Some(param_string) = param_option {
-                if let Ok(value) = param_string.parse::<u32>() {
-                    if value > 0 && value < 300 {
-                        answer += &format!(
-                            "{} ({}) : {}\n",
-                            function_name,
-                            value,
-                            generalized_mean(data, value)
-                        );
-                    } else {
-                        answer += &error(format!(
-                            "'{}' ({}) : value must be more 0 and less 300\n",
-                            function_name,
-                            param_string,
-                        ));
-                    }
-                } else {
-                    answer += &error(format!(
-                        "'{}' ({}) : can't parse to u32\n",
-                        function_name,
-                        param_string,
-                    ));
-                }
-            } else {
-                answer += &format!("{} (def: {}) : {}\n",
+    if_some_macro!(data, parametrs, param_string :
+        if let Ok(value) = param_string.parse::<u32>() {
+            if value > 0 && value < 300 {
+                answer += &format!(
+                    "{} ({}) : {}\n",
                     function_name,
-                    GENERALIZED_MEAN_DEFOULT_VALUE,
-                    generalized_mean(data, GENERALIZED_MEAN_DEFOULT_VALUE)
+                    value,
+                    generalized_mean(data, value)
                 );
+            } else {
+                answer += &error(format!(
+                    "'{}' ({}) : value must be more 0 and less 300\n",
+                    function_name,
+                    param_string,
+                ));
             }
+        } else {
+            answer += &error(format!(
+                "'{}' ({}) : can't parse to u32\n",
+                function_name,
+                param_string,
+            ));
         }
-    }
-    /*
-    else {
-        answer += &format!(
-            "{} (-) : -\n",
+        =>
+        answer += &format!("{} (def: {}) : {}\n",
             function_name,
-        );
-    }
-    */
+            GENERALIZED_MEAN_DEFOULT_VALUE,
+            generalized_mean(data, GENERALIZED_MEAN_DEFOULT_VALUE)
+        )
+    );
     
     answer
 }
@@ -117,39 +135,37 @@ fn call_arithmetic_geometric_mean(data: &[i64], parametrs: &Vec<Option<&str>>) -
     let mut answer = String::new();
     let function_name: String = function_string_name!(arithmetic_geometric_mean);
     
-    if data.len() > 0 {
-        for param_option in parametrs {
-            if let Some(param_string) = param_option {
-                if let Ok(value) = param_string.parse::<u32>() {
-                    if value > 50 && value < 10000 {
-                        answer += &format!("{} ({}) : \n", function_name, value);
-                        arithmetic_geometric_mean(data, value)
-                            .iter()
-                            .enumerate()
-                            .for_each(|(i, x)| answer += &format!("    {} {} : {}\n", data[i], data[i + 1], *x));
-                    } else {
-                        answer += &error(format!(
-                            "'{}' ({}) : value must be more 50 and less 10000\n",
-                            function_name,
-                            param_string,
-                        ));
-                    }
-                } else {
-                    answer += &error(format!(
-                        "'{}' ({}) : can't parse to u32\n",
-                        function_name,
-                        param_string,
-                    ));
-                }
-            } else {
-                answer += &format!("{} (def: {}) : \n", function_name, ARITHMETIC_GEOMETRIC_MEAN_DEFOULT_VALUE);
-                arithmetic_geometric_mean(data, ARITHMETIC_GEOMETRIC_MEAN_DEFOULT_VALUE)
+    if_some_macro!(data, parametrs, param_string :
+        if let Ok(value) = param_string.parse::<u32>() {
+            if value > 50 && value < 10000 {
+                answer += &format!("{} ({}) : \n", function_name, value);
+                arithmetic_geometric_mean(data, value)
                     .iter()
                     .enumerate()
                     .for_each(|(i, x)| answer += &format!("    {} {} : {}\n", data[i], data[i + 1], *x));
+            } else {
+                answer += &error(format!(
+                    "'{}' ({}) : value must be more 50 and less 10000\n",
+                    function_name,
+                    param_string,
+                ));
             }
+        } else {
+            answer += &error(format!(
+                "'{}' ({}) : can't parse to u32\n",
+                function_name,
+                param_string,
+            ));
         }
-    }
+        =>
+        {
+            answer += &format!("{} (def: {}) : \n", function_name, ARITHMETIC_GEOMETRIC_MEAN_DEFOULT_VALUE);
+            arithmetic_geometric_mean(data, ARITHMETIC_GEOMETRIC_MEAN_DEFOULT_VALUE)
+                .iter()
+                .enumerate()
+                .for_each(|(i, x)| answer += &format!("    {} {} : {}\n", data[i], data[i + 1], *x));
+        }
+    );
     
     answer
 }
@@ -158,39 +174,37 @@ fn call_modified_arithmetic_geometric_mean(data: &[i64], parametrs: &Vec<Option<
     let mut answer = String::new();
     let function_name: String = function_string_name!(modified_arithmetic_geometric_mean);
     
-    if data.len() > 0 {
-        for param_option in parametrs {
-            if let Some(param_string) = param_option {
-                if let Ok(value) = param_string.parse::<u32>() {
-                    if value > 50 && value < 10000 {
-                        answer += &format!("{} ({}) : \n", function_name, value);
-                        modified_arithmetic_geometric_mean(data, value)
-                            .iter()
-                            .enumerate()
-                            .for_each(|(i, x)| answer += &format!("    {} {} : {}\n", data[i], data[i + 1], *x));
-                    } else {
-                        answer += &error(format!(
-                            "'{}' ({}) : value must be more 50 and less 10000\n",
-                            function_name,
-                            param_string,
-                        ));
-                    }
-                } else {
-                    answer += &error(format!(
-                        "'{}' ({}) : can't parse to u32\n",
-                        function_name,
-                        param_string,
-                    ));
-                }
-            } else {
-                answer += &format!("{} (def: {}) : \n", function_name, MODIFIED_ARITHMETIC_GEOMETRIC_MEAN_DEFOULT_VALUE);
-                modified_arithmetic_geometric_mean(data, MODIFIED_ARITHMETIC_GEOMETRIC_MEAN_DEFOULT_VALUE)
+    if_some_macro!(data, parametrs, param_string :
+        if let Ok(value) = param_string.parse::<u32>() {
+            if value > 50 && value < 10000 {
+                answer += &format!("{} ({}) : \n", function_name, value);
+                modified_arithmetic_geometric_mean(data, value)
                     .iter()
                     .enumerate()
                     .for_each(|(i, x)| answer += &format!("    {} {} : {}\n", data[i], data[i + 1], *x));
+            } else {
+                answer += &error(format!(
+                    "'{}' ({}) : value must be more 50 and less 10000\n",
+                    function_name,
+                    param_string,
+                ));
             }
+        } else {
+            answer += &error(format!(
+                "'{}' ({}) : can't parse to u32\n",
+                function_name,
+                param_string,
+            ));
         }
-    }
+        =>
+        {
+            answer += &format!("{} (def: {}) : \n", function_name, MODIFIED_ARITHMETIC_GEOMETRIC_MEAN_DEFOULT_VALUE);
+            modified_arithmetic_geometric_mean(data, MODIFIED_ARITHMETIC_GEOMETRIC_MEAN_DEFOULT_VALUE)
+                .iter()
+                .enumerate()
+                .for_each(|(i, x)| answer += &format!("    {} {} : {}\n", data[i], data[i + 1], *x));
+        }
+    );
 
     answer
 }
@@ -205,40 +219,36 @@ fn call_quasi_arithmetic_mean(data: &[i64], parametrs: &Vec<Option<&str>>) -> St
         },
     ];
     
-    if data.len() > 0 {
-        for param_option in parametrs {
-            if let Some(param_string) = param_option {
-                if let Ok(value) = param_string.parse::<usize>() {
-                    if value > 0 && value < functions.len() + 1 {
-                        answer += &format!("{} ({}) : {}\n", 
-                            function_name, 
-                            value, 
-                            quasi_arithmetic_mean(data, functions[value - 1])
-                        );
-                    } else {
-                        answer += &error(format!(
-                            "'{}' ({}) : value must be more 1 and less {}\n",
-                            function_name,
-                            param_string,
-                            functions.len() + 1
-                        ));
-                    }
-                } else {
-                    answer += &error(format!(
-                        "'{}' ({}) : can't parse to usize\n",
-                        function_name,
-                        param_string,
-                    ));
-                }
-            } else {
-                answer += &format!("{} (def: {}) : {}\n", 
+    if_some_macro!(data, parametrs, param_string :
+        if let Ok(value) = param_string.parse::<usize>() {
+            if value > 0 && value < functions.len() + 1 {
+                answer += &format!("{} ({}) : {}\n", 
                     function_name, 
-                    QUASI_ARITHMETIC_MEAN_DEFOULT_VALUE, 
-                    quasi_arithmetic_mean(data, functions[QUASI_ARITHMETIC_MEAN_DEFOULT_VALUE - 1])
+                    value, 
+                    quasi_arithmetic_mean(data, functions[value - 1])
                 );
+            } else {
+                answer += &error(format!(
+                    "'{}' ({}) : value must be more 1 and less {}\n",
+                    function_name,
+                    param_string,
+                    functions.len() + 1
+                ));
             }
+        } else {
+            answer += &error(format!(
+                "'{}' ({}) : can't parse to usize\n",
+                function_name,
+                param_string,
+            ));
         }
-    }
+        =>
+        answer += &format!("{} (def: {}) : {}\n", 
+            function_name, 
+            QUASI_ARITHMETIC_MEAN_DEFOULT_VALUE, 
+            quasi_arithmetic_mean(data, functions[QUASI_ARITHMETIC_MEAN_DEFOULT_VALUE - 1])
+        )
+    );
     
     answer
 }
@@ -248,39 +258,35 @@ fn call_truncated_mean(data: &[i64], parametrs: &Vec<Option<&str>>) -> String {
     let mut answer = String::new();
     let function_name: String = function_string_name!(truncated_mean);
     
-    if data.len() > 0 {
-        for param_option in parametrs {
-            if let Some(param_string) = param_option {
-                if let Ok(value) = param_string.parse::<f64>() {
-                    if value > 0.0 && value < 0.49 {
-                        answer += &format!("{} ({}) : {}\n", 
-                            function_name, 
-                            value, 
-                            truncated_mean(data, value)
-                        );
-                    } else {
-                        answer += &error(format!(
-                            "'{}' ({}) : value must be more 0.0 and less 0.49\n",
-                            function_name,
-                            param_string,
-                        ));
-                    }
-                } else {
-                    answer += &error(format!(
-                        "'{}' ({}) : can't parse to f64\n",
-                        function_name,
-                        param_string,
-                    ));
-                }
-            } else {
-                answer += &format!("{} (def: {}) : {}\n", 
+    if_some_macro!(data, parametrs, param_string :
+        if let Ok(value) = param_string.parse::<f64>() {
+            if value > 0.0 && value < 0.49 {
+                answer += &format!("{} ({}) : {}\n", 
                     function_name, 
-                    TRUNCATED_MEAN_DEFOULT_VALUE, 
-                    truncated_mean(data, TRUNCATED_MEAN_DEFOULT_VALUE)
+                    value, 
+                    truncated_mean(data, value)
                 );
+            } else {
+                answer += &error(format!(
+                    "'{}' ({}) : value must be more 0.0 and less 0.49\n",
+                    function_name,
+                    param_string,
+                ));
             }
+        } else {
+            answer += &error(format!(
+                "'{}' ({}) : can't parse to f64\n",
+                function_name,
+                param_string,
+            ));
         }
-    }
+        =>
+        answer += &format!("{} (def: {}) : {}\n", 
+            function_name, 
+            TRUNCATED_MEAN_DEFOULT_VALUE, 
+            truncated_mean(data, TRUNCATED_MEAN_DEFOULT_VALUE)
+        )
+    );
     
     answer
 }
@@ -290,39 +296,35 @@ fn call_winsorizing_mean(data: &[i64], parametrs: &Vec<Option<&str>>) -> String 
     let mut answer = String::new();
     let function_name: String = function_string_name!(winsorizing_mean);
     
-    if data.len() > 0 {
-        for param_option in parametrs {
-            if let Some(param_string) = param_option {
-                if let Ok(value) = param_string.parse::<f64>() {
-                    if value > 0.0 && value < 0.49 {
-                        answer += &format!("{} ({}) : {}\n", 
-                            function_name, 
-                            value, 
-                            winsorizing_mean(data, value)
-                        );
-                    } else {
-                        answer += &error(format!(
-                            "'{}' ({}) : value must be more 0.0 and less 0.49\n",
-                            function_name,
-                            param_string,
-                        ));
-                    }
-                } else {
-                    answer += &error(format!(
-                        "'{}' ({}) : can't parse to f64\n",
-                        function_name,
-                        param_string,
-                    ));
-                }
-            } else {
-                answer += &format!("{} (def: {}) : {}\n", 
+    if_some_macro!(data, parametrs, param_string :
+        if let Ok(value) = param_string.parse::<f64>() {
+            if value > 0.0 && value < 0.49 {
+                answer += &format!("{} ({}) : {}\n", 
                     function_name, 
-                    WINSORIZING_MEAN_DEFOULT_VALUE, 
-                    winsorizing_mean(data, WINSORIZING_MEAN_DEFOULT_VALUE)
+                    value, 
+                    winsorizing_mean(data, value)
                 );
+            } else {
+                answer += &error(format!(
+                    "'{}' ({}) : value must be more 0.0 and less 0.49\n",
+                    function_name,
+                    param_string,
+                ));
             }
+        } else {
+            answer += &error(format!(
+                "'{}' ({}) : can't parse to f64\n",
+                function_name,
+                param_string,
+            ));
         }
-    }
+        =>
+        answer += &format!("{} (def: {}) : {}\n", 
+            function_name, 
+            WINSORIZING_MEAN_DEFOULT_VALUE, 
+            winsorizing_mean(data, WINSORIZING_MEAN_DEFOULT_VALUE)
+        )
+    );
     
     answer
 }
